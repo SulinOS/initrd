3f1233a585b30068bcf2d20953749072e8aaf142
Ali Rıza KESKİN <parduscix@yandex.ru>
2020-04-12 13:09:56 +0000

    usb sorunu çözüldü

--- src/scripts/02-modules.sh
+++ src/scripts/02-modules.sh
@@ -3,3 +3,5 @@ debug "Listing kernel modules"
 find  /lib/modules/ | sed "s/.*\///g" | grep "\.ko$" | sed "s/.ko$/ &/g" | sed "s/^/modprobe /g"> /load_modules.sh
 msg "Trying to load kernel modules"
 sh /load_modules.sh 1>/dev/null 2>&1
+msg "Waiting for kernel modules"
+sleep 3
