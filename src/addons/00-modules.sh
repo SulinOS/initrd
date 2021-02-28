2d90cde9c60aafe329246edd0e1b0b0e7abd6248
Your Name <you@example.com>
2021-02-28 16:51:18 +0000

    block directory check

--- src/addons/00-modules.sh
+++ src/addons/00-modules.sh
@@ -3,18 +3,20 @@
 [ ! -n "$MODDIR"  ] && MODDIR=/lib/modules/${KERNELVER}
 [ ! -n "$OUTPUT"  ] && OUTPUT=/boot/initrd.img-${KERNELVER}
 [ -d $MODDIR ] || err "Module directory not found" "-> $MODDIR"
-
 debug "Kernel Version" "${KERNELVER}"
 debug "Module Directory" "${MODDIR}"
 mkdir -p ${WORKDIR}/${MODDIR}
 if [ "$allmodule" == true ] && [ "$skipglibc" != "true" ] && [ "$skipudev" != "true" ]; then
 	cp -prf ${MODDIR}/* ${WORKDIR}/${MODDIR}
 elif [ "$minimal" == true ]; then
-     cp -prf ${MODDIR}/kernel/{crypto,fs,lib,block} ${WORKDIR}/${MODDIR}
-     cp -prf ${MODDIR}/kernel/drivers/input/{keyboard,serio} ${WORKDIR}/${MODDIR}
-     cp -prf ${MODDIR}/kernel/drivers/{block,ata,md,mmc,firewire} ${WORKDIR}/${MODDIR}
-     cp -prf ${MODDIR}/kernel/drivers/{scsi,pcmcia,virtio} ${WORKDIR}/${MODDIR}
-     cp -prf ${MODDIR}/kernel/drivers/usb/ ${WORKDIR}/${MODDIR}
+	cp -prf ${MODDIR}/kernel/{crypto,fs,lib,block} ${WORKDIR}/${MODDIR}
+	cp -prf ${MODDIR}/kernel/drivers/input/{keyboard,serio} ${WORKDIR}/${MODDIR}
+	cp -prf ${MODDIR}/kernel/drivers/{ata,md,mmc,firewire} ${WORKDIR}/${MODDIR}
+	if [ -d ${MODDIR}/kernel/drivers/block ] ; then 
+		cp -prf ${MODDIR}/kernel/drivers/block/ ${WORKDIR}/${MODDIR}
+	fi
+	cp -prf ${MODDIR}/kernel/drivers/{scsi,pcmcia,virtio} ${WORKDIR}/${MODDIR}
+	cp -prf ${MODDIR}/kernel/drivers/usb/ ${WORKDIR}/${MODDIR}
 else
 	debug "Install main modules"
 	cp -prf ${MODDIR}/kernel/{crypto,fs,lib} ${WORKDIR}/${MODDIR}
